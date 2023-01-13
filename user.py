edebf821cbfb4239c2dec193e687eabd77fc58ba
627km <rudals_jeon@naver.com>
2023-01-13 16:26:07 +0900

    feat: user.py update
    Update user features

--- user.py
+++ user.py
@@ -1,38 +1,75 @@
 from random import randint
 from time import sleep
 
-dice_num  = randint(1, 6+1)
-user_result = 0
-user_total_result = 0
-goal = 10
+class Dice:
 
-def roll_dice():
-    dice_num = randint(1, 6+1)
-    return dice_num
+    def __init__(self):
+        self.surface = [1,2,3,4,5,6]
+        self.current_value = 0
+    
+    def roll_and_get_value(self):
+        random_number = randint(0, 5)
+        self.current_value = self.surface[random_number]
+        return self.current_value
+    def check_value_is_one(self):
+        return self.current_value is 1
 
-print('Welcome! We start pig-dice-game')
-sleep(1)
-button = input("Enter 'r' button > ")
+class User:
 
-if button != 'r':
-    print('Enter correct button')
-elif button == 'r':    
-    while True:
-        if button == 'r':
-            dice_num = roll_dice()
-            print('dice_num =',dice_num)
-            if dice_num != 1:
-                user_result += dice_num
-                print('Your current result =', user_result)
-                if user_total_result + user_result >= goal:
-                    print('You Win!!')
-                    break
-                button = input('Roll(r) or Stop(s) ? >')
-            else:
-                user_result = 0
-                break
-        elif button == 's':
-            break
-        
-    user_total_result += user_result
-    print('Your total result =',user_total_result)
+    def __init__(self):
+        self.sub_sum = 0
+        self.total_sum = 0
+    
+    def add_sub_num(self, value):
+        self.sub_sum += value
+
+    def accumulate_sub_sum_to_total_sum(self):
+        self.total_sum += self.sub_sum
+    
+    def get_sub_sum(self):
+        return self.sub_sum
+
+    def get_total_sum(self):
+        return self.total_sum
+
+    def reset_sub_sum(self):
+        self.sub_sum = 0
+    
+    def check_total_sum_is_over_fifyy(self):
+        return self.total_sum >= 50
+    
+    def add_and_check_sub_sum_and_total_sum_is_over_fifty(self):
+        return self.total_sum + self.sub_sum >= 50
+
+class UserPlay:
+
+    def __init__(self):
+        self.dice = Dice()
+        self.user = User()
+
+    def start_play(self):
+        self.user.reset_sub_sum()
+        print('사용자가 플레이를 시작합니다.')
+        print(f'사용자의 누적 합 >> {self.user.get_total_sum()}점')
+
+        while True:
+            button = input('Roll(r) or Stop(s) ? >')
+            sleep(1)
+            if button == 'r':
+                dice_value = self.dice.roll_and_get_value()
+                print(f'주사위를 굴려서 나온 숫자 >> {dice_value}')
+                if self.dice.check_value_is_one():
+                    print(f'주사위의 값이 1이 나와 컴퓨터에게 턴을 넘깁니다. 누적합: {self.user.get_total_sum()}')
+                    self.user.reset_sub_sum()
+                    return True
+                else:
+                    self.user.add_sub_num(dice_value)
+                    print(f'Sub(Current) 누적합 >> {self.user.get_sub_sum()}') 
+                    if self.user.add_and_check_sub_sum_and_total_sum_is_over_fifty():
+                        self.user.accumulate_sub_sum_to_total_sum()
+                        print(f'총 누적 합이 50이상입니다.\n 사용자의 승리로 게임을 종료합니다.! 총 누적 합 >> {self.user.get_total_sum()}')
+                        return False
+            elif button == 's':
+                self.user.accumulate_sub_sum_to_total_sum()
+                print(f'사용자의 턴이 종료되었습니다. 컴퓨터의 턴입니다. 사용자 누적 합 >> {self.user.get_total_sum()}')
+                return True
