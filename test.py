de2631a94df770b911bcc7b5ba0e32a9a6e88c1b
627km <rudals_jeon@naver.com>
2023-01-13 16:27:05 +0900

    feat: update test.py
    update test.py

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 from computer import ComputerAutoPlay
+from user import UserPlay
 
 if __name__ == "__main__":
     
     computer = ComputerAutoPlay()
+    user = UserPlay()
     
     while True:
-        
+        if user.start_play():
+           print('사용자의 paly 종료...\n\n\n')
+
+        else:
+            break
+
+
         if computer.start_auto_play():
-            print("컴퓨터의 auto play 종료...")
+            print("컴퓨터의 auto play 종료...\n\n\n")
         else:
             break
